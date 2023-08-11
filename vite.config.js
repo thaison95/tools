3dbb6625f760fd8797d09df89f1fd5b1ae3252d4
Toan Trieu <toan.trieu@trustingsocial.com>
2023-08-11 16:06:24 +0700

    Fix inlining web manifest

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,10 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
-      manifest: {
-        name: "TS Passio Orders",
-        short_name: "PassioOrders",
-        description: "Order now, pay later",
-        theme_color: "#a6cc3a",
-        icons: [
-          {
-            "src": "/passio_512.png",
-            "type": "image/png",
-            "sizes": "512x512"
-          },
-          {
-            "src": "/passio_192.png",
-            "type": "image/png",
-            "sizes": "192x192"
-          }
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef
@@ -37,5 +12,7 @@ export default defineConfig({
     },
   },
   "server.hmr.overlay": false,
-  publicDir: "assets",
+  build: {
+    assetsInlineLimit: 0,
+  }
 });
