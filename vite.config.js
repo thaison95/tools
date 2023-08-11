662fb8b05ab16a3cd165a62359810ddb51aff033
Toan Trieu <toan.trieu@trustingsocial.com>
2023-08-11 15:43:27 +0700

    Add vite-plugin-pwa

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,35 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "TS Passio Orders",
+        short_name: "PassioOrders",
+        description: "Order now, pay later",
+        theme_color: "#a6cc3a",
+        icons: [
+          {
+            "src": "/passio_512.png",
+            "type": "image/png",
+            "sizes": "512x512"
+          },
+          {
+            "src": "/passio_192.png",
+            "type": "image/png",
+            "sizes": "192x192"
+          }
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef
@@ -12,5 +37,5 @@ export default defineConfig({
     },
   },
   "server.hmr.overlay": false,
-  publicDir: 'assets'
-})
+  publicDir: "assets",
+});
