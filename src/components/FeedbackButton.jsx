bc0af26435338ba60578d77db4ea007e89a4b2f6
Toan Trieu <toan.trieu@trustingsocial.com>
2023-10-04 17:35:53 +0700

    Fix icon feedback

--- src/components/FeedbackButton.jsx
+++ src/components/FeedbackButton.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Button } from "./ui/button";
+import { PenLine } from "lucide-react";
 
 const FEEDBACK_FORM_URL = "https://forms.gle/6WERuXNNbp8GxKeq8";
 
 const FeedbackButton = () => {
   return (
-    <Button variant="outline" size="sm" asChild>
+    <Button variant="outline" size="icon" asChild>
       <a href={FEEDBACK_FORM_URL} rel="noopener noreferrer" target="_blank" className="text-xs">
-        Feedback
+        <PenLine />
       </a>
     </Button>
   );
